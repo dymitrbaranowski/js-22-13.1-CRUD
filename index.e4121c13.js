fe3473ede8acc992b60532b7b8620ac9859bfd07
dymitrbaranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-14 15:55:31 +0000

    Deploying to gh-pages from @ dymitrbaranowski/js-22-13.1-Crud@584081d420cd797b1c9c848eb1a87878c5cfb0dc 🚀

--- index.e4121c13.js
+++
@@ -1,2 +0,0 @@
-!function(){function t(t){var n="".concat("http://localhost:4040","/books/").concat(t);return fetch(n,{method:"DELETE"}).then((function(t){return t.json()}))}t(13),t(12)}();
-//# sourceMappingURL=index.e4121c13.js.map
