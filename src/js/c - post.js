584081d420cd797b1c9c848eb1a87878c5cfb0dc
Dymitr Baranowski <125156435+dymitrbaranowski@users.noreply.github.com>
2024-11-14 17:55:08 +0200

    fox3

--- src/js/c - post.js
+++ src/js/c - post.js
@@ -30,19 +30,21 @@ function addBook(book) {
   return fetch(`${BASE_URL}/books`, options).then(res => res.json());
 }
 
-addBook({
-  title: 'Тестовая книга по CSS',
-  author: 'Я',
-  genres: ['CSS'],
-  rating: 9,
-}).then(renderBook);
+// addBook({
+//   title: 'Тестовая книга по CSS',
+//   author: 'Я',
+//   genres: ['CSS'],
+//   rating: 9,
+// })
+//   .then(renderBook)
+//   .catch(error => console.log(error));
 
-addBook({
-  title: 'Тестовая книга по HTML',
-  author: 'Я',
-  genres: ['HTML'],
-  rating: 7,
-}).then(renderBook);
+// addBook({
+//   title: 'Тестовая книга по HTML',
+//   author: 'Я',
+//   genres: ['HTML'],
+//   rating: 7,
+// }).then(renderBook);
 
 function renderBook(book) {
   console.log('Пришел ответ от бекенда можно рисовать');
